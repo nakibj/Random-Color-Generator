5c34405f3e0497caa447b674a826814bcb02d883
nakibj <161792116+nakibj@users.noreply.github.com>
2026-01-28 01:29:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const pageBackground = document.querySelector("body");
 
 btn.addEventListener("click", generate);
 
-let s = "#";
 let arr = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
 function generate(){
+    let s = "#";
     for(let i =0; i<6; i++){
         let index = Math.floor(Math.random() * 16);
 
