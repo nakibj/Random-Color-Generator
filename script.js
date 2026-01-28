40f9c9660a33624e3d365f6534b00de52e21bc06
nakibj <161792116+nakibj@users.noreply.github.com>
2026-01-28 01:30:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pageBackground = document.querySelector("body");
 
 btn.addEventListener("click", generate);
 
-let arr = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+const arr = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
 function generate(){
     let s = "#";
